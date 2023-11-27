a363de5c6d6f631722d89d2de2a7b7f46f0cc15b
M-Facey <faceymaxal17@gmail.com>
2023-11-27 01:46:03 -0500

    chore: remove print statement

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from config import PG_USERNAME, PG_PASSWORD, DB_HOST, DB_NAME
 
-print(PG_PASSWORD, PG_PASSWORD, DB_NAME)
 engine = create_engine(
     F"postgresql://{PG_USERNAME}:{PG_PASSWORD}@{DB_HOST}/{DB_NAME}",
     echo=True
